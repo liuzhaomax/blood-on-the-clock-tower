9ffd9950e456ed8451003181d48b3203597a4227
LIUZHAO <liuzhaomax@163.com>
2024-03-05 15:06:38 +0800

    ICP

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom"
 import {blood} from "../../utils/blood/blood"
 import {sleep} from "../../utils/time"
 import config from "../../config/config"
+import ICP from "../../assets/images/ICP.png"
 
 if (localStorage.getItem("PlayerID") === null) {
     localStorage.setItem("PlayerID", genShortUUID())
@@ -229,6 +230,19 @@ function Home() {
                     :
                     <div></div>
                 }
+            </div>
+            <div id="Warn-health">
+                <p>健康游戏忠告</p>
+                <p>抵制不良游戏，拒绝盗版游戏。</p>
+                <p>注意自我保护，谨防受骗上当。</p>
+                <p>适度游戏益脑，沉迷游戏伤身。</p>
+                <p>合理安排时间，享受健康生活。</p>
+            </div>
+            <div id="ICP">
+                <img src={ICP} alt="ICP"/>
+                <a href="https://beian.mps.gov.cn/#/query/webSearch" target="\_blank" style={{color: "white",textDecoration:"none"}}>
+                    津ICP备2024011617号-1
+                </a>
             </div>
             <Drawer
                 title="创建房间"
